1aa8a4bd4c366f27e276e26cbe3b67e8958c8ea3
Yaagna <yaagnamodi@gmail.com>
2022-11-11 00:07:04 +0530

    commit

---
+++ half_adder_testbench.v
@@ -0,0 +1,24 @@
+//Test bench for halfadder
+`include "half_adder.v"
+
+module testbench;
+    reg a, b;
+    wire sum, carry;
+
+    half_adder add1(a,b,sum,carry);
+
+    initial 
+        begin
+            $dumpfile("half_adder.vcd");
+            $dumpvars(0, testbench);
+            a = 0; b = 0;
+            #1 
+            a = 0; b = 1;
+            #1
+            a = 1; b = 0;
+            #1
+            a = 1; b = 1;
+            #1
+            a = 1; b = 1;
+        end
+endmodule
