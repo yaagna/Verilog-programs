09ad06d589edf54f636eb80e3145420eb39afb55
Yaagna <yaagnamodi@gmail.com>
2022-11-20 18:58:31 +0530

    commit

--- full adder/full_adder.v
+++ full adder/full_adder.v
@@ -1,10 +1,15 @@
 module full_adder(a, b, cin, sum, cout);
     input a, b, cin;
     output sum, cout;
-    wire m,p
-    xor(s,a,b,cin);
-    xor(m,a,b);
-    and(p,a,b);
-    and(v,m,cin);
-    or(cout,p,v);
+    wire w, x, y;
+    half_adder h1(w, x, a, b);
+    half_adder h2(sum, y, w, cin);
+    or(cout, x, y);
+endmodule
+
+module half_adder(S, C, A, B);
+    input A, B;
+    output S, C;
+    xor(S, A, B);
+    and(C, A, B);
 endmodule
--- full adder/full_adder_testbench.v
+++ full adder/full_adder_testbench.v
@@ -1,29 +1,33 @@
+//Test bench for halfadder
 `include "full_adder.v"
 
 module testbench;
     reg a, b, cin;
     wire sum, cout;
-
-    //Initiate Unit Under Test(UUT)
-    full uut(
-        .sum(sum),
-        .cout(cout),
-        .a(a),
-        .b(b),
-        .cin(cin)
-    );
-
+    
+    full_adder add1(a, b, cin, sum, cout);
+    
     initial 
     begin
-        $dumpfile("full_adder.vcd");
-        $dumpvars(0, testbench);
-        a = 1'b0;
-        b = 1'b0;
-        cin = 1'b0;
+      $dumpfile("full_adder.vcd");
+      $dumpvars(0, testbench);
+      a = 1'b0; b = 1'b0; cin = 1'b0; 
+      #2;
+      a = 1'b0; b = 1'b1; cin = 1'b0;
+      #2;
+      a = 1'b1; b = 1'b0; cin = 1'b0;
+      #2;
+      a = 1'b1; b = 1'b1; cin = 1'b0;
+      #2;
+      a = 1'b0; b = 1'b0; cin = 1'b1;
+      #2;
+      a = 1'b0; b = 1'b1; cin = 1'b1;
+      #2;
+      a = 1'b1; b = 1'b0; cin = 1'b1;
+      #2;
+      a = 1'b1; b = 1'b1; cin = 1'b1;
     end
-    always #2 a = a+1'b1;
-    always #3 b = b+1'b1;
-    always #4 cin = cin+1'b1;
-    initial $monitor($time,"sum=%b, cout=%b",sum,cout);
-    initial #50, $finish;
 endmodule
+
+//iverilog -o full_adder_testbench.vvp full_adder_testbench.v
+//vvp full_adder_testbench.vvp
