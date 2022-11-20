6bf620a78f8bab28c5d7780661e916ab8ad31e24
Yaagna <yaagnamodi@gmail.com>
2022-11-20 22:24:17 +0530

    commit

---
+++ half subtactor/half_sub.v
@@ -0,0 +1,8 @@
+module half_sub(a, b, diff, borrow);
+    input a, b;
+    output diff, borrow;
+    wire inverse_a;
+    xor x1(diff, a, b);
+    not n1(inverse_a,a);
+    and a1(borrow, inverse_a, b);
+endmodule
