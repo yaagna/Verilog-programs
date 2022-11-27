9bc9490e7f605c84a22375a3f58b94ee4a4d8c1d
Yaagna <yaagnamodi@gmail.com>
2022-11-27 21:05:22 +0530

    commit

--- multiplexer/2 to 1 mux/mux2_1.v
+++ multiplexer/2 to 1 mux/mux2_1.v
@@ -1,10 +1,10 @@
-module (i0, i1, s, y);
+module mux2_1(i0, i1, s, y);
     input wire i0, i1, s;
-    output y;
+    output reg y;
 
     always @(i0 or i1 or s)
     begin
-        if (s)
+        if(s)
             y = i1;
         else
             y = i0;
--- multiplexer/2 to 1 mux/mux2_1_testbench.v
+++ multiplexer/2 to 1 mux/mux2_1_testbench.v
@@ -2,12 +2,14 @@
 `include "mux2_1.v"
 
 module testbench;
-    wire out;
+    wire y;
     reg i0, i1, s;
-    mux2_1 name(.y(out), .i0(i0), .i1(i1), .s(s));
+    mux2_1 name(.y(y), .i0(i0), .i1(i1), .s(s));
 
     initial 
     begin
+        $dumpfile("mux2_1.vcd");
+        $dumpvars(0, testbench);
         i0 = 1'b0;
         i1 = 1'b0;
         s = 1'b0;
@@ -18,7 +20,7 @@ module testbench;
     always #20 i1 = ~i1;
     always #10 s = ~s;
     always @(i0 or i1 or s)
-    $monitor("At time = %t, Output = %d", $time, out);
+    $monitor("At time = %t, Output = %d", $time, y);
 endmodule
 
 //iverilog -o mux2_1_testbench.vvp mux2_1_testbench.v
