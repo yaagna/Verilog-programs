fae7673ad838b582245fb72f2de2d593ee340ea8
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-11-27 09:16:16 +0000

    commit

--- multiplexer/2 to 1 mux/mux2_1.v
+++ multiplexer/2 to 1 mux/mux2_1.v
@@ -2,6 +2,12 @@ module (i0, i1, s, y);
     input wire i0, i1, s;
     output y;
 
-    
+    always @(i0 or i1 or s)
+    begin
+        if (s)
+            y = i1;
+        else
+            y = i0;
+    end    
 
 endmodule
