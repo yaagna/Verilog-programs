7189fcc5576fa317765543fa48363b0d23d08f51
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-11-27 15:13:30 +0000

    commit

--- multiplexer/2 to 1 mux/mux2_1.v
+++ multiplexer/2 to 1 mux/mux2_1.v
@@ -1,3 +1,5 @@
+// Verilog 2 to 1 Multiplexer program using behavioural model
+
 module (i0, i1, s, y);
     input wire i0, i1, s;
     output y;
--- multiplexer/4 to 1 mux/mux4_1.v
+++ multiplexer/4 to 1 mux/mux4_1.v
@@ -1,3 +1,5 @@
+// Verilog 4 to 1 Multiplexer program using behavioural model
+
 module mux4_1(input [3:0] i, output y, input [1:0] s);
 
 endmodule
