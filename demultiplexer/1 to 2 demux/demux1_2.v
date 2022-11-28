505d13486fc3a134933530116d96e00fcc79a242
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-11-28 17:36:17 +0000

    commit

--- demultiplexer/1 to 2 demux/demux1_2.v
+++ demultiplexer/1 to 2 demux/demux1_2.v
@@ -1,6 +1,20 @@
 // Verilog 1 to 2 Demultiplexer program
 
-module demux1_2();
-    
+module demux1_2(i, s, y);
+    input i, s;
+    output reg [1:0] y;
 
+    always @ (*)
+    begin
+        if (s == 1'b0)
+            begin
+                y[0] = i;
+                y[1] = 1'b0;
+            end
+        else
+            begin
+                y[0] = 1'b0;
+                y[1] = i;
+            end
+    end
 endmodule
