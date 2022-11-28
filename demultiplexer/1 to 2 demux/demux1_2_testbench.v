56c9b3145752ad54c2e5286889a7c0e95eb7e6db
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-11-28 17:39:22 +0000

    commit

--- demultiplexer/1 to 2 demux/demux1_2_testbench.v
+++ demultiplexer/1 to 2 demux/demux1_2_testbench.v
@@ -1,7 +1,11 @@
 // Test bench for 1 to 2 demultiplexer
+`include "demux1_2.v"
 
 module testbench;
+    reg i, s;
+    wire [1:0] y;
 
+    demux
 endmodule
 
 //iverilog -o demux1_2_testbench.vvp demux1_2_testbench.v
