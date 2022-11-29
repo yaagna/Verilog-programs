edb60d75cb058d3376d0a3c523bd7f06271b0a60
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-11-29 16:27:21 +0000

    commit

--- Carry look ahead adder/4-bit CLA/cla4.v
+++ Carry look ahead adder/4-bit CLA/cla4.v
@@ -9,6 +9,11 @@ module cla4(a, b, cin, cout, y);
     wire [3:0] g, p;                // generate and propagate signals
     wire [4:0] c;                   // intermediate carry signals between stages
 
+    assign c[0] = cin;
     assign y[0] = a[0] ^ b[0] ^ cin;
-    
+
+    assign c[1] = ((a[0] & b[0]) | ((a[0] | b[0]) & c[0]));
+    assign y[1] = a[1] ^ b[1] ^ c[1];
+
+    assign c[2] = ((a[1] & b[1]) | ((a[1] | b[1]) & ((a[0] & b[0]) | ((a[0] | b[0]) & c[0]))));
 endmodule
