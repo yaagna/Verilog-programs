53657299b705b81cffe87fbe7b93cbfdee6de4b2
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-12-28 16:07:24 +0000

    commit

--- ALU/4 bit ALU/logical_unit.v
+++ ALU/4 bit ALU/logical_unit.v
@@ -10,7 +10,17 @@ module logical
 
 always @(posedge clk) begin
 
-
+    case (instruction)
+        3'b000: log_out = a & b;
+        3'b001: log_out = a | b;
+        3'b010: log_out = a ^ b;
+        3'b011: log_out = ~(a | b);
+        3'b100: log_out = ~(a & b);
+        3'b101: log_out = ~(a ^ b);
+        3'b110: log_out = (a>b) ? 4'b1111 : 4'b0000;
+        3'b111: log_out = (a==b) ? 4'b1111 : 4'b0000;
+        default: log_out = a;
+    endcase
 
 end
 
