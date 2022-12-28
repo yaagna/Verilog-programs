fe5216749d8926e140984d7c6f25a37383fa7a6a
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2022-12-28 16:19:40 +0000

    commit

--- ALU/4 bit ALU/alu4.v
+++ ALU/4 bit ALU/alu4.v
@@ -9,4 +9,13 @@ module alu;
     output reg [N-1:0] alu_out;
 );
 
+    wire [N-1:0] lu, au;
+
+    // initialize the sub blocks
+    Logical Logic(.clk(clk), .A(A), .B(B), .instruction(instruction[M-2:0]), .LU_out(LU));
+    Arithematic arith(.clk(clk), .A(A), .B(B), .instruction(instruction[M-2:0]), .AU_out(AU));
+
+    always @*
+        alu_out <= (instruction[M-1]==1) ? lu : au;
+
 endmodule
