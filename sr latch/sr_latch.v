81e1e5830a75263a7eb515b19e4617c11c63f147
yaagna <yaagnamodi@gmail.com>
2023-01-18 21:34:36 +0530

    commit

--- sr latch/sr_latch.v
+++ sr latch/sr_latch.v
@@ -1,16 +1,21 @@
-module srff(sr,clk,q,qb);
-
-    input [1:0] sr;
-    input clk;
-    output reg q, qb;
-
-    always @(posedge clk)begin
-        case(sr)
-        2'b00: q = q;
-        2'b01: q = 0;
-        2'b10: q = 1;
-        2'b11: q = 1'bz;
-        endcase
+module sr_flipflop (
+  input clk, rst_n,
+  input s,r,
+  output reg q,
+  output q_bar
+  );
+  
+  // always@(posedge clk or negedge rst_n) // for asynchronous reset
+  always@(posedge clk) begin // for synchronous reset
+    if(!rst_n) q <= 0;
+    else begin
+      case({s,r})
+        2'b00: q <= q;    // No change
+        2'b01: q <= 1'b0; // reset
+        2'b10: q <= 1'b1; // set
+        2'b11: q <= 1'bx; // Invalid inputs
+      endcase
     end
-    assign qb = ~q;    
+  end
+  assign q_bar = ~q;
 endmodule
