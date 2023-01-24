cedc70501b7321d33a05117d3dcd2073c691c093
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2023-01-24 15:53:46 +0000

    commit

--- jk latch/jk_latch.v
+++ jk latch/jk_latch.v
@@ -3,4 +3,14 @@ module jk_latch(j, k, clk, reset, q);
     input j, k, clk, reset;
     output reg q;
 
+    always @(posedge clk)
+      if(reset == 1)
+        q <= 0;
+      else
+        case ({j,k})
+          2'b00: q <= q;
+          2'b01: q <= 0;
+          2'b10: q <= 1;
+          2'b11: q <= ~q;
+        endcase
 endmodule
