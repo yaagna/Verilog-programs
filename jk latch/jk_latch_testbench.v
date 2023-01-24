bdfd652b925cc16abe8b47bb570af8b56eb3d122
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2023-01-24 16:00:18 +0000

    commit

--- jk latch/jk_latch_testbench.v
+++ jk latch/jk_latch_testbench.v
@@ -9,5 +9,28 @@ module testbench;
     output reg q;
 
     initial clk = 0;
+    always #5 clk = ~clk;
 
+    jk_latch jk0 (.j(j), .k(k), .clk(clk), .reset(reset), .q(q));
+
+    initial begin
+        reset <= 1;
+        #10 reset = 0;
+    end
+
+    initial 
+    begin
+        $dumpfile("jk_latch.vcd");
+        $dumpvars(0, testbench);
+
+        j <= 1;
+        k <= 0;
+
+        #20 j <= 0;
+            k <= 0;
+        #20 j <= 0;
+            k <= 1;
+        #20 j <= 1;
+            k <= 1;
+    end
 endmodule
