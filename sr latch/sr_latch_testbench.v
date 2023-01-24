58a4ccdb4a72289c6be66456f8df4ec6ddea66a3
Yaagna K. Modi <63107418+yaagna@users.noreply.github.com>
2023-01-24 15:13:21 +0000

    commit

--- sr latch/sr_latch_testbench.v
+++ sr latch/sr_latch_testbench.v
@@ -8,4 +8,10 @@ module testbench;
     reg clk;
     reg reset;
 
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    sr_latch sr0 (.s(s), .r(r), .clk(clk), .reset(reset), .q(q));
+    
+
 endmodule
