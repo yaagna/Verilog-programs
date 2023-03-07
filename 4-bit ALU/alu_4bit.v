17ef0f83b6fd6b344768caa49afe0d890bb64830
yaagna <yaagnamodi@gmail.com>
2023-03-07 14:14:15 +0530

    reset function added

--- 4-bit ALU/alu_4bit.v
+++ 4-bit ALU/alu_4bit.v
@@ -1,18 +1,33 @@
 // Verilog code for 4-bit ALU
 
-module alu (input clk, input [3:0] a, input [3:0] b, input [1:0] sel, output [3:0] out, output overflow);
-    reg [3:0] out;
-    reg overflow;
+module alu (input clk,
+    input reset,
+    input [3:0] a, 
+    input [3:0] b, 
+    input [1:0] sel,
+    output [3:0] out,
+    output overflow);
+
+    reg [3:0] result;
+    wire [4:0] temp;
+
+    assign out = result;
+    assign temp = {1'b0,a} + {1'b0,b};
+    assign overflow = temp[4];
+
     always @(posedge clk)
-        case (sel)
-            2'b00: out <= a + b;
-            2'b01: out <= a - b;
-            2'b10: out <= a * b;
-            2'b11: out <= a / b;
-        endcase
-    always @(posedge clk)
-        if (out > 4'b1111)
-            overflow = 1;
-        else
-            overflow = 0;
+        begin
+            if (reset == 1'b0)
+                case (sel)
+                   2'b00: result <= a + b;
+                   2'b01: result <= a - b;
+                   2'b10: result <= a * b;
+                   2'b11: result <= a / b;
+                endcase 
+            else
+                result <= 4'b0000;
+ 
+        end
+
 endmodule
+
--- 4-bit ALU/alu_4bit_testbench.v
+++ 4-bit ALU/alu_4bit_testbench.v
@@ -3,5 +3,63 @@
 `include "alu_4bit.v"
 
 module testbench;
+    reg clk;
+    reg reset;
+    reg [3:0] a, b;
+    reg [1:0] sel;
+    wire [3:0] out;
+    wire overflow;
+
+    alu uut (clk, reset, a, b, sel, out, overflow);
+
+    initial clk = 0;
+    always #1 clk = ~clk;
+      
+    initial begin
+
+        $dumpfile("alu_4bit.vcd");
+        $dumpvars(0, testbench);
+
+        reset = 1'b0;
+        a = 4'b0000;
+        b = 4'b0000;
+        sel = 2'b00;
+
+        #10;
+        a = 4'b0001;
+        b = 4'b0001;
+        sel = 2'b00;
+
+        #10;
+        a = 4'b1111;
+        b = 4'b1111;
+        sel = 2'b00;
+
+        #2;
+        reset = 1'b1;
+
+        #2;
+        reset = 1'b0;
+        a = 4'b0000;
+        b = 4'b0000;
+        sel = 2'b01;
+
+        #10;
+        a = 4'b0001;
+        b = 4'b0001;
+        sel = 2'b01;
+
+        #10;
+        a = 4'b0001;
+        b = 4'b0011;
+        sel = 2'b01;
+
+
+        #10$finish;
+
+    end
 
 endmodule
+
+//iverilog -o alu_4bit_testbench.vvp alu_4bit_testbench.v
+//vvp alu_4bit_testbench.vvp
